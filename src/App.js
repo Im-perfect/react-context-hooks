7e9bde51b4bb8f82dec4e82f202446ed67f22d3f
Im-perfect <zhangke.kerri@gmail.com>
2020-01-02 16:36:11 +0100

    using contextType to access context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import ThmeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-     
+      <ThmeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThmeContextProvider>
     </div>
   );
 }
