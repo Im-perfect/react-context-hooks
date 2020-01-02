db501a439e761b0f0e17f37b0c4ddae5c9367ecf
Im-perfect <zhangke.kerri@gmail.com>
2020-01-02 16:41:23 +0100

    using context consumer to access context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,28 @@ export default class Navbar extends Component {
   static contextType = ThemeContext;
 
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div>
-        <nav style={{ background: theme.ui, color: theme.syntax }}>
-          <h1>Context App</h1>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </div>
+        //benefits
+        // - can consume multiple contexts in one component
+        // - can also be used in functional components (contextType can only be used in class component)
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div>
+              <nav style={{ background: theme.ui, color: theme.syntax }}>
+                <h1>Context App</h1>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact</li>
+                </ul>
+              </nav>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
